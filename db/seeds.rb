a5125e7f75f6822c9a24d776261c4314821ffbb0
Allison Sessé <allisonsesse@gmail.com>
2021-02-22 17:55:16 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Category.destroy_all
+Activity.destroy_all
 
-users = User.create([{username: "martin", email: "martin@gmail.com", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "emile", email: "emile@gmail.com", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}])
-activities = Activity.create([{name: "yoga", location: "bois de la Cambre", date: Date.today, description: "1 hour"}, {name: "climbing", location: "la petite île", date: Date.today, description: "blabla"}])
-categories = Category.create([{name: "sport"}, {name: "cultural activities"}])
+users = User.create!([{username: "martin", email: "martin@gmail.com", password: "123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "emile", email: "emile@gmail.com", password:"123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}])
+categories = Category.create!([{name: "sport"}, {name: "cultural activities"}])
+activities = Activity.create!([{name: "yoga", location: "bois de la Cambre", date: Date.today, description: "1 hour", user: users.first, category: categories.first}, {name: "climbing", location: "la petite île", date: Date.today, description: "blabla", user: users.last, category: categories.last}])
