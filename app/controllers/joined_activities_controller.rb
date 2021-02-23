19aeae93301a572bb9c7e8ee76e207ccd65b604f
arthur bary <arthurbary@gmail.com>
2021-02-23 15:53:06 +0100

    created controllers_joined_activity create+new

--- app/controllers/joined_activities_controller.rb
+++ app/controllers/joined_activities_controller.rb
@@ -1,2 +1,20 @@
 class JoinedActivitiesController < ApplicationController
+  def new
+    @joined_acitivty = JoinedActivity.new
+  end
+
+  def create
+    @joined_acitivty = JoinedActivity.new(joined_activity_params)
+
+    if @joined_acitivty.save
+      redirect_to activity_path(@activity), notice: "you have joined this activity"
+    else
+      render :new
+    end
+  end
+
+  def joined_activity_params
+    params.require(:joined_acitivty).permit(:current_user, :activity_id)
+  end
+
 end
