979abee83a3b7b5b7fcef2829056563e5d35b2d5
Alexandre Rojon <alexandre.rojon@gmail.com>
2021-02-23 11:26:20 +0100

    New feature in controller and views complete, but needs CSS

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,11 @@ class ActivitiesController < ApplicationController
   def show
     @activity = Activity.find(params[:id])
   end
+
+
+  def new
+    #check_authorized
+    @activity = Activity.new
+  end
+
 end
