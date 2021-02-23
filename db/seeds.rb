f7f20b83058db7d9c935a1a3896662ba790f5ce3
Allison Sessé <allisonsesse@gmail.com>
2021-02-23 15:51:01 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ User.destroy_all
 Category.destroy_all
 Activity.destroy_all
 
-users = User.create!([{username: "martin", email: "martin@gmail.com", password: "123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "emile", email: "emile@gmail.com", password:"123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}])
-categories = Category.create!([{name: "sport"}, {name: "cultural activities"}])
+users = User.create!([{username: "martin", email: "martin@gmail.com", password: "123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "emile", email: "emile@gmail.com", password:"123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "seb", email: "seb@gmail.com", password:"123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "arthur", email: "arthur@gmail.com", password:"123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "french"}, {username: "alex", email: "alex@gmail.com", password:"123456", birthdate: (Date.today - 20.years), age: 20, gender: "male", language: "english"}])
+categories = Category.create!([{name: "sport"}, {name: "cultural"}, {name: "drink"}, {name: "restaurant"}, {name: "culunary"}, {name: "mechanical"}, {name: "manual"}, {name: "non-classified"}])
 activities = Activity.create!([{name: "yoga", location: "bois de la Cambre", date: Date.today, description: "1 hour", user: users.first, category: categories.first}, {name: "climbing", location: "la petite île", date: Date.today, description: "blabla", user: users.last, category: categories.last}])
+joined_activities = JoinedActivity.create!(status: "upcoming", user: users.first, activity: activities.first)
