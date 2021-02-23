32af1ae514e6fffd520cb6330e37ffa1c2052f60
Alexandre Rojon <alexandre.rojon@gmail.com>
2021-02-23 14:17:43 +0100

    Create in controller complete.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,12 +1,32 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [ :show ]
+
   def show
     @activity = Activity.find(params[:id])
   end
-
 
   def new
     #check_authorized
     @activity = Activity.new
   end
 
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+    if @activity.save!
+      redirect_to @activity, notice: "Your activity has been saved."
+    else
+      render :new
+    end
+  end
+
+
+private
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:name, :location, :description, :date, :category_id)
+  end
 end
